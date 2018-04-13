dab3db5b7cfb1b0193dd8deb30bce8c697ed64dc
Roger Miret <rogerxaic@users.noreply.github.com>
2018-04-13 10:00:37 +0200

    added IE11 compat

--- app/client/src/services/TranslateService.js
+++ app/client/src/services/TranslateService.js
@@ -5,6 +5,10 @@ app.config(['$translateProvider', function ($translateProvider) {
 
     let browser_langs = window.navigator.languages;
     let found = false;
+    if (!browser_langs) {
+        browser_langs = [];
+        browser_langs.push(window.navigator.language);
+    }
     browser_langs.forEach(function (browser_lang) {
         if(!found) {
             let lang = browser_lang.substr(0, 2);
